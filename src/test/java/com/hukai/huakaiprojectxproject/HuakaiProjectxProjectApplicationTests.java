b827afbb0d00c1f66217ae3b5a098a2db500117c
hk <liuzhihong@duiba.com.cn>
2023-07-02 08:42:27 +0800

    ct

---
+++ src/test/java/com/hukai/huakaiprojectxproject/HuakaiProjectxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hukai.huakaiprojectxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuakaiProjectxProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
