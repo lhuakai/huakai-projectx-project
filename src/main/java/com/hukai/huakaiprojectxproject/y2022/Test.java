ddf6ab1d788f13293ef49fdd6ad2c43fd65a2d31
hk <liuzhihong@duiba.com.cn>
2023-07-02 11:30:22 +0800

    ct1

---
+++ src/main/java/com/hukai/huakaiprojectxproject/y2022/Test.java
@@ -0,0 +1,13 @@
+package com.hukai.huakaiprojectxproject.y2022;
+
+/**
+ * @author: huakai
+ * @createTime: 2023/07/02 11:30
+ * @description: todo
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
